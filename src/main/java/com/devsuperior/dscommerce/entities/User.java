e5085775bb51034857ec4e289ebd182f14822352
Isabel Siva <bell.cristina.pereiradasilva@gmail.com>
2023-01-03 16:49:47 -0300

    create relationship into entities

--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -3,6 +3,8 @@ package com.devsuperior.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_user")
@@ -15,6 +17,8 @@ public class User {
     private String phone;
     private LocalDate birthDate;
     private String password;
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User(){}
 
